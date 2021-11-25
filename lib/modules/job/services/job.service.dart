8c8c7e84429127cb99c3f28ecd889ce89fad01cb
Cata <catab212@gmail.com>
2021-11-25 18:35:29 +0200

    Added request for details in job service

--- lib/modules/job/services/job.service.dart
+++ lib/modules/job/services/job.service.dart
@@ -19,40 +19,29 @@ class JobService {
     return _dio;
   }
 
-  Future<JobModel> getDetails(int jobId) async => JobModel(
-          id: 0,
-          name: 'Name',
-          description:
-              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          images: [
-            'https://picsum.photos/id/1/600',
-            'https://picsum.photos/id/2/600',
-            'https://picsum.photos/id/3/600',
-            'https://picsum.photos/id/4/600',
-            'https://picsum.photos/id/5/600',
-          ]);
-// initializeDio() // TODO uncomment this when we have jobs
-// .get('/Job/Details', queryParameters: {'id': jobId})
-// .then((response) => JobModel.fromJson(response.data))
-// .catchError(
-//   (error) {
-//     switch (error.runtimeType) {
-//       case DioError:
-//         switch (error.response?.statusCode) {
-//           case null:
-//             throw ConnectionTimedOutException();
-//           case 400:
-//             throw ServerSideValidationException.fromJson(
-//               error.response.data,
-//             );
-//           case 404:
-//             throw NotFoundException();
-//           default:
-//             throw UnexpectedException();
-//         }
-//       default:
-//         throw UnexpectedException();
-//     }
-//   },
-// );
+  Future<JobModel> getDetails(int jobId) async =>
+      initializeDio() // TODO uncomment this when we have jobs
+          .get('/Job/Details', queryParameters: {'id': jobId})
+          .then((response) => JobModel.fromJson(response.data))
+          .catchError(
+            (error) {
+              switch (error.runtimeType) {
+                case DioError:
+                  switch (error.response?.statusCode) {
+                    case null:
+                      throw ConnectionTimedOutException();
+                    case 400:
+                      throw ServerSideValidationException.fromJson(
+                        error.response.data,
+                      );
+                    case 404:
+                      throw NotFoundException();
+                    default:
+                      throw UnexpectedException();
+                  }
+                default:
+                  throw UnexpectedException();
+              }
+            },
+          );
 }
