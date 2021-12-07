db21910e2d9bd1136433f095c8146d6e9bfc9f42
Vlad rares Raducu <Vladrares.Raducu@cloudflight.io>
2021-12-07 10:59:32 +0200

    Mock profile screen

--- lib/modules/user/screens/user_profile.screen.dart
+++ lib/modules/user/screens/user_profile.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:project/modules/shared/widgets/loading_indicator.widget.dart';
+import 'package:project/modules/user/models/user.model.dart';
 import 'package:project/modules/user/providers/user_profile.provider.dart';
 import 'package:project/modules/user/widgets/user_profile.widget.dart';
 import 'package:provider/provider.dart';
@@ -20,29 +21,37 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   void initState() {
     super.initState();
 
-    WidgetsBinding.instance!.addPostFrameCallback((_) {
-      final userId = ModalRoute.of(context)?.settings.arguments as int?;
-      if (userId == null) Navigator.pop(context);
-      Provider.of<UserProfileProvider>(context, listen: false).getUser(
-        userId!,
-      );
-    });
+    // WidgetsBinding.instance!.addPostFrameCallback((_) {
+    //   final userId = ModalRoute.of(context)?.settings.arguments as int?;
+    //   if (userId == null) Navigator.pop(context);
+    //   Provider.of<UserProfileProvider>(context, listen: false).getUser(
+    //     userId!,
+    //   );
+    // });
   }
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<UserProfileProvider>(context);
-    if (provider.error != null) {
-      Navigator.pop(context); //TODO error screen
-    }
-
+    // final provider = Provider.of<UserProfileProvider>(context);
+    // if (provider.error != null) {
+    //   Navigator.pop(context); //TODO error screen
+    // }
     return Scaffold(
-        body: provider.loading
+        /*body: provider.loading
             ? Center(
                 child: LoadingIndicator(
                   type: LoadingIndicatorType.Page,
                 ),
               )
-            : UserProfile(activeUser: provider.activeUser!));
+            : UserProfile(activeUser: provider.activeUser!));*/
+        body: UserProfile(
+            activeUser: User(
+                id: 69,
+                firstName: "Vlad-Rares",
+                lastName: "Raducu",
+                dateOfBirth: DateTime.utc(2000, 6, 8),
+                email: "vlad101vlad@gmail.com",
+                profilePicture:
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/40/Mangekyou_Sharingan_Kakashi.svg/2048px-Mangekyou_Sharingan_Kakashi.svg.png")));
   }
 }
--- lib/modules/user/widgets/user_profile.widget.dart
+++ lib/modules/user/widgets/user_profile.widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:project/config/theme.config.dart';
 import 'package:project/modules/user/models/user.model.dart';
-import 'package:project/modules/user/providers/user_profile.provider.dart';
-import 'package:provider/provider.dart';
 
 class UserProfile extends StatefulWidget {
   final User activeUser;
@@ -17,12 +16,59 @@ class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
 
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("User Profile"),
-      ],
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        vertical: ThemeConfig.of(context).appMargin,
+        horizontal: ThemeConfig.of(context).appMargin,
+      ),
+      child: Column(
+        children: [
+          Text("User Profile", style: ThemeConfig.of(context).headline4),
+          SizedBox(
+            height: 72,
+            width: 72,
+            child: ClipOval(
+              child: Image(
+                  image: (widget.activeUser.profilePicture == null
+                      ? AssetImage('assets/blank-profile-picture.png')
+                      : NetworkImage(widget.activeUser.profilePicture!))
+                  as ImageProvider),
+            ),
+          ),
+          Text(
+            "First Name",
+            style: ThemeConfig.of(context).headline5,
+          ),
+          Text(
+            widget.activeUser.firstName!,
+            style: ThemeConfig.of(context).headline6,
+          ),
+          Text(
+            "Last Name",
+            style: ThemeConfig.of(context).headline5,
+          ),
+          Text(
+            widget.activeUser.lastName!,
+            style: ThemeConfig.of(context).headline6,
+          ),
+          Text(
+            "Date of birth",
+            style: ThemeConfig.of(context).headline5,
+          ),
+          Text(
+            widget.activeUser.dateOfBirth!.toIso8601String(),
+            style: ThemeConfig.of(context).headline6,
+          ),
+          Text(
+            "Email",
+            style: ThemeConfig.of(context).headline5,
+          ),
+          Text(
+            widget.activeUser.email!,
+            style: ThemeConfig.of(context).headline6,
+          ),
+        ],
+      ),
     );
   }
 }
